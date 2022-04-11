2af158d0ad11fc3d728f9dab5203d5b75478a0e4
Ratikanata <ratikantadash2001@gmail.com>
2022-04-11 18:52:13 +0530

    basic setup for backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) => {
+  res.send('welcome to my new app')
+})
+
+app.listen(port, () => {
+  console.log(`Backend app listening on port ${port}`)
+})
