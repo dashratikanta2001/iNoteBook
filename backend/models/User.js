97eb14b2a359663e1b80a1ada0def2582502d9b9
Ratikanata <ratikantadash2001@gmail.com>
2022-04-11 19:35:20 +0530

    storing data into the Database using mongoose

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const { Schema } = mongoose;
 const UserSchema = new Schema({
     name:{
         type: String,
@@ -15,9 +15,9 @@ const UserSchema = new Schema({
         required: true
     },
     date:{
-        type: String,
-        required: true
+        type: Date,
+        default : Date.now
     }
   });
 
-  module.exports = mongoose.model('user',UserSchema);
+  module.exports = mongoose.model('user',UserSchema)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/', (req, res)=>{
-    obj={
-        a:'rati',
-        number:1234
-    }
-    res.json(obj)
+//create a User using POST "/api/auth/". Doesn't require Auth
+router.post('/', (req, res)=>{
+    
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
 })
 
 module.exports = router
